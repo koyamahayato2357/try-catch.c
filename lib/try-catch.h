ec00135e760b118b2f6997902ffddbfc87521b8d
koyamahayato2357 <sundongxiaoshan202@gmail.com>
2024-09-22 17:47:42 +0900

    feature: retry

--- lib/try-catch.h
+++ lib/try-catch.h
@@ -16,11 +16,13 @@ void EXCEPTION_H_cl(int **g);
   for (int EXCEPTION_H_GENUNIQTOK(cnt) = 1; EXCEPTION_H_GENUNIQTOK(cnt);)      \
     for (int *EXCEPTION_H_CAT(EXCEPIOTN_H_g_, cnt)                             \
              __attribute__((cleanup(EXCEPTION_H_cl))) = &EXCEPTION_H_nest;     \
-      if ((errcode = setjmp(EXCEPTION_H_jb[EXCEPTION_H_nest++])) == 0)
          EXCEPTION_H_GENUNIQTOK(cnt); EXCEPTION_H_GENUNIQTOK(cnt) = 0)         \
+      if ((errcode = setjmp(EXCEPTION_H_jb[EXCEPTION_H_nest++])) == 0 ||       \
+          errcode == ERR_RETRY)
 
 #define try EXCEPTION_H_TRY(__COUNTER__)
 #define catch(e) else if (errcode == (e))
 #define catchor(e) else if (errcode & (e))
 #define catchany else
 #define throw(e) longjmp(EXCEPTION_H_jb[--EXCEPTION_H_nest], e);
+#define retry throw(ERR_RETRY)
